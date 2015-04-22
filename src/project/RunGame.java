8aef2f7a0da9749c6219933a2ca718aa91114dba
melfurd <jchi@oakland.edu>
2015-04-22 09:22:40 -0400

    added dynamic texas holdem

--- src/project/RunGame.java
+++ src/project/RunGame.java
@@ -2,7 +2,7 @@ package project;
 
 public class RunGame {
 	public static void main(String args[]){
-		TableTop t = new TableTop(new TexasHoldem2(5));
+		TableTop t = new TableTop(new TexasHoldem2(2));
 		//TableTop t = new TableTop(new War());
 		
 		//TableTop t = new TableTop("Poke");
